e12dcb426e1e7daaa6547b4d862694011eaa30c1
connor <caschm@gmail.com>
2024-05-15 17:20:24 -0400

    add to company detail page

--- frontend/src/CompanyDetail.jsx
+++ frontend/src/CompanyDetail.jsx
@@ -1,11 +1,33 @@
+import { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
+import JoblyApi from "../api";
 
 function CompanyDetail() {
+    const [isLoading, setIsLoading] = useState(true);
+    const [company, setCompany] = useState([]);
     const { companyHandle } = useParams();
+
+    /* useEffect to get company details from API upon inital render */
+
+    useEffect(() => {
+        async function getCompany() {
+            let response = await await JoblyApi.getCompany(companyHandle);
+            // console.log(response);
+            setCompany(response);
+            return response;
+        }
+        getCompany();
+        setIsLoading(false);
+    }, []);
+
+    if (isLoading) {
+        return <p>Loading &hellip;</p>;
+    }
+
     return (
-        <div>
-            <h1>Company Detail</h1>
-            <p>{companyHandle}</p>
+        <div className="CompanyDetail">
+            <h1>{company.name}</h1>
+            <p>{company.description}</p>
         </div>
     );
 }
