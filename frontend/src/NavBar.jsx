81e29159fbb88801491d2fc065af9d9fda9f0038
connor <caschm@gmail.com>
2024-05-15 15:57:02 -0400

    install reactstrap

--- frontend/src/NavBar.jsx
+++ frontend/src/NavBar.jsx
@@ -1,8 +1,27 @@
 import { NavLink } from "react-router-dom";
+import { Navbar, Nav, NavItem } from "reactstrap";
 import "./NavBar.css";
 
 function NavBar() {
-    return <div></div>;
+    return (
+        <Navbar expand="md">
+            <NavLink exact to="/" className="navbar-brand">
+                Jobly
+            </NavLink>
+
+            <Nav className="ml-auto" navbar>
+                <NavItem>
+                    <NavLink to="/snacks">Snacks</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink to="/drinks">Drinks</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink to="/new">Add Item</NavLink>
+                </NavItem>
+            </Nav>
+        </Navbar>
+    );
 }
 
 export default NavBar;
