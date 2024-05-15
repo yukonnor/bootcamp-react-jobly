aba95d7134ec0b49106f0edfa080f6b60fba043f
connor <caschm@gmail.com>
2024-05-15 12:04:16 -0400

    test api connection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import JoblyApi from "../api.js";
 import "./App.css";
 
 function App() {
     const [count, setCount] = useState(0);
+
+    /* useEffect to get menue items from db.json upon inital render */
+
+    useEffect(() => {
+        async function getJobs() {
+            let response = await await JoblyApi.request("jobs");
+            console.log(response);
+        }
+        getJobs();
+    }, []);
 
     return (
         <>
